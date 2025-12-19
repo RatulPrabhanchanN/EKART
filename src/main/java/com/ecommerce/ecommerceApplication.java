5bd786015a764a38d986247a4bde5d51e2d869f5
RATUL PRABHANCHAN N <ratulprabhanchann@gmail.com>
2025-12-19 16:14:31 +0530

    Added user.java , userRepository.java , register.html files  and modified ecommerceApplication.java, application.properties

---
+++ src/main/java/com/ecommerce/ecommerceApplication.java
@@ -0,0 +1,9 @@
+package com.ecommerce;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringApplication;
+
+@SpringApplication
+public class EcommerceApplication {
+    SpringApplication.run(EcommerceApplication.class,args);
+    
+}
---
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.ecommerce.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.ecommerce.entity.User;
+
+public class UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByUsername(String username);
+    public Optional<User> findByEmail(String email);
+    public boolean existsByUsername(String username);
+    public boolean existsByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.username = ?1 OR u.email = ?2")
+    public Optional<User> findByUsernameOrEmail(String username, String email);
+    // @Query("SELECT u FROM User u WHERE u.username = :username OR u.email = :email")
+    // Optional<User> findByUsernameOrEmail(@Param("username") String username, @Param("email")String email);
+
+}
